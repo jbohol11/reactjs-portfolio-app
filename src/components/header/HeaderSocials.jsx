0167b13e5f08e9bd5eaae62779b089b384fd4785
Jhun Bohol <101481990+jbohol11@users.noreply.github.com>
2022-03-13 08:26:10 +0800

    Update HeaderSocials.jsx

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -6,7 +6,7 @@ const HeaderSocials = () => {
     return (
         <div className="header__socials">
             <a href="https://linkedin.com" target="_blank" rel='noreferrer'><BsLinkedin /></a>
-            <a href="https://github.com/jrbohol11" target="_blank" rel='noreferrer'><BsGithub /></a>
+            <a href="https://github.com/jbohol11" target="_blank" rel='noreferrer'><BsGithub /></a>
             <a href="https://facebook.com/jrbohol11" target="_blank" rel='noreferrer'><BsFacebook /></a>
         </div>
     )
